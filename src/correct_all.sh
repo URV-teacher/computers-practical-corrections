8038cfadc3718c64bd3b819a80f2b240d4bd619f
Aleix Mariné-Tena <aleix.marine@estudiants.urv.cat>
2024-10-24 07:47:19 +0200

    UPDATED: data parsing

--- src/correct_all.sh
+++ src/correct_all.sh
@@ -3,13 +3,25 @@
 # Argument 1: Relative or absolute path to the json file that contains the data for the correction
 correct_one()
 {
-  cat "$1" |
+  # Parse data
+  name="$(cat "$1" | ${JQ} .name | tr -d "\"")"
+  email="$(cat "$1" | ${JQ} .email | tr -d "\"")"
+  repository="$(cat "$1" | ${JQ} .repository | tr -d "\"")"
+  role="$(cat "$1" | ${JQ} .role | tr -d "\"")"
+  test1="$(cat "$1" | ${JQ} .test1 | tr -d "\"")"
+  test2="$(cat "$1" | ${JQ} .test2 | tr -d "\"")"
+  fusion="$(cat "$1" | ${JQ} .fusion | tr -d "\"")"
+
+  # Perform clone and checkout code
 }
 
 
 main()
 {
+  correct_one ${PROJECT_FOLDER}/data/examples/example.json
+
   # End with read to avoid the terminal screen disappear
+  echo "Press any key to continue"
   read
 }
 
