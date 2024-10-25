bc2af71eb95b89d27f98a348ef5c5b83a7d25570
Aleix Mariné-Tena <aleix.marine@estudiants.urv.cat>
2024-10-25 08:07:42 +0200

    UPDATED: correct all, first steps

--- src/correct_all.sh
+++ src/correct_all.sh
@@ -12,13 +12,37 @@ correct_one()
   test2="$(cat "$1" | ${JQ} .test2 | tr -d "\"")"
   fusion="$(cat "$1" | ${JQ} .fusion | tr -d "\"")"
 
+  echo "
+************************************************************************************************************************
+* Student: ${name} (${email})
+* Role: ${role}
+************************************************************************************************************************"
+
   # Perform clone and checkout code
+  git clone "${DNI}@${GIT_SERVER}:${repository}" "${PROJECT_FOLDER}/tmp/${repository}-${role}"
+
+  # Force git to work in the cloned repo
+  export GIT_DIR="${PROJECT_FOLDER}/tmp/${repository}-${role}/.git"
+  export GIT_WORK_TREE="${PROJECT_FOLDER}/tmp/${repository}-${role}/.git"
+
+  git show -s --format=%ci ${test1}
 }
 
 
 main()
 {
-  correct_one ${PROJECT_FOLDER}/data/examples/example.json
+  echo "
+************************************************************************************************************************
+* Computers course 2024-2025
+* Corrections for practical exercise phase 1
+************************************************************************************************************************"
+
+  for file in "${PROJECT_FOLDER}"/data/*.json; do
+    if [[ -f "${file}" ]]; then
+      correct_one "${file}"
+    fi
+  done
+
 
   # End with read to avoid the terminal screen disappear
   echo "Press any key to continue"
@@ -30,5 +54,8 @@ export PROJECT_FOLDER
 PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")/.." &>/dev/null && pwd)"
 
 JQ="${PROJECT_FOLDER}\lib\jq-windows-amd64.exe"
+GIT_SERVER="git.deim.urv.cat"
+
+source "${PROJECT_FOLDER}/.env"
 
 main "$@"
